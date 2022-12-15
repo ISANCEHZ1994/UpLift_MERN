e2e95114fd6d11654f111ef37d2371a016101745
Israel Sanchez <Israel.pro1994@gmail.com>
2022-12-15 15:43:08 -0600

    learning and adding socket.io to small chat feature

--- up-lift-back/config/server.js
+++ up-lift-back/config/server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./db');
 const http = require("http");
+const cors = require('cors');
 const { Server } = require("socket.io");
 
+// const connectDB = require('./db');
 // connectDB();
 app.use(express.json({ extended: false }));
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server ( server, {
+    cors: {
+        // which url origin will be called
+        origin: "http://localhost:3005",
+        methods: [ "GET", "POST" ]
+    }
+});
+
+io.on("connection", ( socket ) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED", socket.id);
+    })
+});
 
 app.get('/', (req, res) => res.send('This is the API and it. Is. RUNNING!!'));
-
 const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`SERVER started on ${PORT}`));
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 //Define Routes
 // app.use('/api/users', require('./routes/api/users'));
 // app.use('/api/auth', require('./routes/api/auth'));
 // app.use('/api/profile', require('./routes/api/profile'));
 // app.use('/api/posts', require('./routes/api/posts'));
-
-
--- up-lift/src/components/feedPage.js
+++ up-lift/src/components/feedPage.js
@@ -2,11 +2,18 @@ import React, { Fragment } from "react";
 import io from "socket.io-client";
 import { useState } from "react";
 
+const socket = io.connect("http://localhost:3005");
+
 const FeedPage = () => {
 
     return (
         <Fragment>
-            <h2>this should be where we are see the feed</h2>
+            <div className="profile-grid my-1">
+                <h2>this should be where we are see the feed</h2>
+                <div className="profile-exp bg-white p-2">
+
+                </div>
+            </div>            
         </Fragment>
     )
 };
