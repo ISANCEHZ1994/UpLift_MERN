b412532b4b89ccf0b67683f7cecea5ada8ad6805
Israel Sanchez <Israel.pro1994@gmail.com>
2023-01-03 22:23:30 -0600

    working on profile routes

--- up-lift/src/components/welcomePage.js
+++ up-lift/src/components/welcomePage.js
@@ -6,22 +6,33 @@ import { Link } from 'react-router-dom';
 const WelcomePage = () => {
 
     return (
-        <section className="landing">
-            <div className="dark-overlay">
-                <div className="landing-inner">
-                    <h1 className="x-large">UpLift</h1>
-                    <p className="lead">
-                        Connect with others who enjoy to be active together. 
-                        Share favorite workouts or motivational videos and
-                         lift each other up to become better than they were before!
-                    </p>
-                    <div className="buttons">
-                        <Link to="/register" className="btn btn-primary">Sign Up</Link>
-                        <Link to="/login" className="btn btn-light">Login</Link>
+        <>
+            <nav class="navbar bg-dark">
+                <h1>
+                    <Link to='/'><FontAwesomeIcon icon={faDumbbell} /> UpLift</Link>
+                </h1>
+                <ul>
+                    <li><Link to="/register">Register</Link></li>                    
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </nav>
+            <section className="landing">
+                <div className="dark-overlay">
+                    <div className="landing-inner">
+                        <h1 className="x-large">UpLift</h1>
+                        <p className="lead">
+                            Connect with others who enjoy to be active together. 
+                            Share favorite workouts or motivational videos and
+                            lift each other up to become better than they were before!
+                        </p>
+                        <div className="buttons">
+                            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+                            <Link to="/login" className="btn btn-light">Login</Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 };
 
